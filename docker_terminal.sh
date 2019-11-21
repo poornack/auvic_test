9b5c78b141d069804b5a208d0481a1b4d39b48be
Poorna Chander Kalidas <poornakalidas@gmail.com>
2019-11-21 02:52:20 -0800

    Run script

--- docker_terminal.sh
+++ docker_terminal.sh
@@ -2,4 +2,4 @@
 time_now=$(date +%s)
 echo $time_now
 
-docker run -it -v //firmware:/root/firmware -e 'TZ=America/Los_Angeles' auvic_firmware bash
+docker run -it -v //firmware:/root/firmware auvic_firmware bash
